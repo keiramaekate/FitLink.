d234e783edad813fd7b5c5749624191f07d6d68c
keiramaekate <keira_mae@live.com>
2021-04-08 18:16:37 +0100

    Segments variables
    PagerAdapter - not working for progress page fragments
    
    Settings dropdown menu not working

---
+++ app/src/main/java/com/example/fitlinkv3/retrofit/Segments.java
@@ -0,0 +1,99 @@
+package com.example.fitlinkv3.retrofit;
+
+public class Segments<distance> {
+
+    private Integer id;
+    private Integer resourceState;
+    private String name;
+    private String activityType;
+    private Integer distance;
+    private Integer averageGrade;
+    private Integer maximumGrade;
+    private Integer elevationHigh;
+    private Integer elevationLow;
+    private Integer startLatlng;
+    private Integer endLatlng;
+    private Integer climbCategory;
+    private Object city;
+    private Object state;
+    private Object country;
+    private Integer createdAt;
+    private Integer updatedAt;
+    private Integer totalElevationGain;
+    private Integer athleteSegmentStats;
+
+    public void setId(Integer id) { this.id = id; }
+
+    public Integer getResourceState(){ return resourceState; }
+
+    public void setResourceState(Integer resourceState) { this.resourceState = resourceState; }
+
+    public String getName(){ return name; }
+
+    public void setName(String Name) { this.name = name; }
+
+    public String getActivityType(){ return activityType; }
+
+    public void setActivityType(String ActivityType) { this.activityType = activityType; }
+
+    public Integer getDistance(){ return distance; }
+
+    public void setDistance(Integer distance){ this.distance = distance; }
+
+    public Integer getAverageGrade() { return averageGrade; }
+
+    public void setAverageGrade(Integer averageGrade){ this.averageGrade = averageGrade; }
+
+    public Integer getMaximumGrade(){ return averageGrade; }
+
+    public void setMaximumGrade(Integer maximumGrade){ this.maximumGrade = maximumGrade; }
+
+    public Integer getElevationHigh(){ return elevationHigh; }
+
+    public void setElevationHigh(Integer elevationHigh){ this.elevationHigh = elevationHigh; }
+
+    public Integer getElevationLow(){ return elevationLow; }
+
+    public void setElevationLow(Integer elevationLow){ this.elevationLow = elevationLow; }
+
+    public Integer getStartLatlng(){ return startLatlng; }
+
+    public void setStartLatlng(Integer startLatlng){ this.startLatlng = startLatlng; }
+
+    public Integer getEndLatlng(){ return endLatlng; }
+
+    public void setEndLatlng(Integer endLatlng){ this.endLatlng = endLatlng; }
+
+    public Integer getClimbCategory(){ return climbCategory; }
+
+    public void setClimbCategory(Integer climbCategory){ this.climbCategory = climbCategory; }
+
+    public Object getCity(){ return city; }
+
+    public void setCity(Object city){ this.city = city; }
+
+    public Object getState(){ return state; }
+
+    public void setState(Object state){ this.state = state; }
+
+    public Object getCountry(){ return country; }
+
+    public void setCountry(Object country){ this.country = country; }
+
+    public Integer getCreatedAt(){ return createdAt; }
+
+    public void setCreatedAt(Integer createdAt){ this.createdAt = createdAt; }
+
+    public Integer getUpdatedAt(){ return updatedAt; }
+
+    public void setUpdatedAt(Integer updatedAt){ this.updatedAt = updatedAt; }
+
+    public Integer getTotalElevationGain(){ return totalElevationGain; }
+
+    public void setTotalElevationGain(Integer totalElevationGain){ this.totalElevationGain = totalElevationGain; }
+
+    public Integer getAthleteSegmentStats(){ return athleteSegmentStats; }
+
+    public void setAthleteSegmentStats(Integer athleteSegmentStats){ this.athleteSegmentStats = athleteSegmentStats; }
+
+}
