72d71e2ff33a59bea9042d5d4727e282cb6da986
keiramaekate <keira_mae@live.com>
2021-04-13 21:02:29 +0100

    removed dependency from gradle to run app

--- app/src/main/java/com/example/fitlinkv3/ChatBot.java
+++ app/src/main/java/com/example/fitlinkv3/ChatBot.java
@@ -4,14 +4,12 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.ibm.cloud.sdk.core.security.IamAuthenticator;
-
 public class ChatBot extends AppCompatActivity {
 
     // generate IAM token using API key
-    IamAuthenticator authenticator = new IamAuthenticator.Builder()
-            .apikey("8v2X6SGbyc0TvRpdDcwLGRaie9llNTh7rdm2Vv2hTNnu\n")
-            .build();
+    //IamAuthenticator authenticator = new IamAuthenticator.Builder()
+            //.apikey("8v2X6SGbyc0TvRpdDcwLGRaie9llNTh7rdm2Vv2hTNnu\n")
+           // .build();
 
     //Create the service instance
     //ChatBot service = new ChatBot(authenticator);
