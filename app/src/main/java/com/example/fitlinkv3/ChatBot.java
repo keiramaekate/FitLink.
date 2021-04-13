f1ecdf413b42d8490a2887f8e339bf8437ea14ea
keiramaekate <keira_mae@live.com>
2021-04-13 20:49:50 +0100

    Link to IBM API SDK attempt

--- app/src/main/java/com/example/fitlinkv3/ChatBot.java
+++ app/src/main/java/com/example/fitlinkv3/ChatBot.java
@@ -4,11 +4,23 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.ibm.cloud.sdk.core.security.IamAuthenticator;
+
 public class ChatBot extends AppCompatActivity {
+
+    // generate IAM token using API key
+    IamAuthenticator authenticator = new IamAuthenticator.Builder()
+            .apikey("8v2X6SGbyc0TvRpdDcwLGRaie9llNTh7rdm2Vv2hTNnu\n")
+            .build();
+
+    //Create the service instance
+    //ChatBot service = new ChatBot(authenticator);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_bot);
+
+
     }
 }
---
+++ app/src/main/java/com/example/fitlinkv3/IBM_Cloud_Service/CloudGenerator.java
@@ -0,0 +1,6 @@
+package com.example.fitlinkv3.IBM_Cloud_Service;
+
+
+public class CloudGenerator {
+
+}
