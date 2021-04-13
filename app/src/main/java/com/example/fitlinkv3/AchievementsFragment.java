5db4aba8f53d60cc2a0d42db3dbc6b3bf354919a
Karim Nagy <kn271@kent.ac.uk>
2021-04-13 23:47:01 +0200

    Fixed achievements
    Added Run and Ride distance achievements

--- app/src/main/java/com/example/fitlinkv3/AchievementsFragment.java
+++ app/src/main/java/com/example/fitlinkv3/AchievementsFragment.java
@@ -77,146 +77,84 @@ public class AchievementsFragment extends Fragment {
                                 if (response != null && response.isSuccessful())
                                 {
                                     ActivityStats stravaresponse = response.body();
-                                    if(stravaresponse !=null)
-                                    {
+                                    if(stravaresponse !=null) {
 
                                         //Get TOTAL ACTIVITY COUNT for achievement
                                         int TotalRunCount = stravaresponse.getAll_run_totals_count();
                                         int TotalRideCount = stravaresponse.getAll_ride_totals_count();
                                         int TotalSwimCount = stravaresponse.getAll_swim_totals_count();
 
-                                        int TotalActivityCount = TotalRunCount+TotalRideCount+TotalSwimCount;
+                                        int TotalActivityCount = TotalRunCount + TotalRideCount + TotalSwimCount;
 
                                         //put if statement here\\
-                                        if (TotalActivityCount >= 10 )
-                                        {
+                                        if (TotalActivityCount >= 10) {
                                             //set image to gold
                                             TotalActivity.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.total_activity_athlete));
                                             Toast.makeText(getContext(), "gold", Toast.LENGTH_LONG).show();
-                                        }
-                                            else  if (TotalActivityCount >= 6 ){
+                                        } else if (TotalActivityCount >= 6) {
                                             //set silver
                                             TotalActivity.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.total_activity_pro));
                                             Toast.makeText(getContext(), "silver", Toast.LENGTH_LONG).show();
                                         }
-                                            //set bronze
-                                            else if (TotalActivityCount < 5 ){
+                                        //set bronze
+                                        else if (TotalActivityCount < 5) {
                                             TotalActivity.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.total_activity_novice));
                                             Toast.makeText(getContext(), "bronze", Toast.LENGTH_LONG).show();
                                         }
+                                            //Get RUN distance for achievement
+                                            Double TotalRunMeters = stravaresponse.getAll_run_totals_distance();
+                                            Double TotalRunMiles = TotalRunMeters * 0.000621371;
+                                            Double RunTotalDistance = TotalRunMiles;
 
+                                            DecimalFormat df = new DecimalFormat("##.##");
+                                            Double RoundedRunDistance = Double.valueOf(df.format(RunTotalDistance));
 
-                                        //Get RUN distance for achievement
-                                        Double TotalRunMeters = stravaresponse.getAll_run_totals_distance();
-                                        Double TotalRunMiles = TotalRunMeters*0.000621371;
-                                        Double RunTotalDistance = TotalRunMiles;
+                                            if (RoundedRunDistance >= 50){
+                                                //set image to gold
+                                                TotalRunDistance.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.gold_run_distance));
+                                            }
+                                            else if(RoundedRunDistance >=40){
+                                                //set image to silver
+                                                TotalRunDistance.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.silver_run_distance));
+                                            }
+                                            else if(RoundedRunDistance >=10){
+                                                //set image to bronze
+                                                TotalRunDistance.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.bronze_run_distance));
+                                            }
+                                            else if (RoundedRunDistance >= 0) {
+                                                //set image to default
+                                                TotalRunDistance.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.default_run_distance));
+                                            }
+                                                // Get RIDE distance for achievement
+                                                Double TotalRideMeters = stravaresponse.getAll_ride_totals_distance();
+                                                Double TotalRideMiles = TotalRideMeters * 0.000621371;
+                                                Double TotalRideMilesDistance = TotalRideMiles;
+                                                //Make TotalDistance to 2 decimal places
+                                                Double RoundedRideDistance = Double.valueOf(df.format(TotalRideMilesDistance));
 
-                                        DecimalFormat df = new DecimalFormat("##.##");
-                                        Double TotalRunDistance = Double.valueOf(df.format(RunTotalDistance));
+                                                if (RoundedRideDistance >= 50){
+                                                    //set image to gold
+                                                    TotalRideDistance.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.gold_ride_distance));
+                                                }
+                                                else if(RoundedRideDistance >=40) {
+                                                    //set image to silver
+                                                    TotalRideDistance.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.silver_ride_distance));
+                                                }
+                                                else if(RoundedRideDistance >=20) {
+                                                    //set image to bronze
+                                                    TotalRideDistance.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.bronze_ride_distance));
+                                                }
+                                                else if(RoundedRideDistance == 0){
+                                                    //set image to default
+                                                    TotalRideDistance.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.default_ride_distance));
 
-                                        //if (TotalRunDistance >= 20) {
-                                            //set image to gold
-                                            //can't set image from string
-                                            //TotalRunDistance.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.gold_Run));
-                                            //Toast.makeText(getContext(), "gold", Toast.LENGTH_LONG).show();
+                                            }
                                         }
-
-                                        // Get RIDE distance for achievement
-                                        Double TotalRideMeters = stravaresponse.getAll_ride_totals_distance();
-                                        Double TotalRideMiles = TotalRideMeters*0.000621371;
-                                        Double RideTotalDistance = TotalRideMiles;
-
-                                        //Make TotalDistance to 2 decimal places
-                                        DecimalFormat df = new DecimalFormat("##.##");
-                                        String RoundedRideTotalDistance = String.valueOf(df.format(RideTotalDistance));
-
-                                        //insert if statement here
-
-
-                                        //Get all-time RUN time_elapsed
-                                        int TotalRunTime = stravaresponse.getAll_run_totals_time();
-                                        int RunTotalTime = TotalRunTime;
-
-                                        //if statement to check run time totals more than or equal to 5 hours
-                                        if (RunTotalTime >= 300){
-                                            //set image to gold
-                                            TotalRunDistance.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.gold_run_time));
-                                            Toast.makeText(getContext(), "gold", Toast.LENGTH_LONG).show();
-                                        }
-
-                                        //if statement to check run time totals more than or equal to than 3 hours
-                                        else if (RunTotalTime >= 180){
-                                            //set image to silver
-                                            TotalRunDistance.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.silver_run_time));
-                                            Toast.makeText(getContext(), "silver", Toast.LENGTH_LONG).show();
-                                        }
-
-                                        // if statement to check the run time totals is less than 2 hours
-                                        else if (RunTotalTime < 120){
-                                            //set image to bronze
-                                            TotalRunDistance.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.bronze_run_time));
-                                            Toast.makeText(getContext(), "brozne", Toast.LENGTH_LONG).show();
-                                        }
-
-                                        //if statement to set image to default when run time total equals 0
-                                        else if (RunTotalTime == 0){
-                                            //set image to default if no data is retrieved 
-                                            TotalRunDistance.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.default_medal));
-                                            Toast.makeText(getContext(), "no data", Toast.LENGTH_LONG).show();
-                                        }
-
-
-                                        // Get all-time RIDE time-elapsed
-                                        int TotalRideTime = stravaresponse.getAll_ride_totals_time();
-                                        int RideTotalTime = TotalRideTime;
-
-                                        //if statement to check RIDE time totals more than or equal to 5 hours
-                                         if (RideTotalTime >= 300){
-                                             //set image to gold
-                                             TotalRideDistance.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.gold_ride_time));
-                                            Toast.makeText(getContext(), "gold", Toast.LENGTH_LONG).show();
-                                         }
-
-                                         //if statement to check RIDE time totals more than or equal to than 3 hours
-                                         else if (RunTotalTime >= 180){
-                                             //set image to silver
-                                            TotalRideDistance.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.silver_ride_time));
-                                            Toast.makeText(getContext(), "silver", Toast.LENGTH_LONG).show();
-                                         }
-
-                                         // if statement to check the RIDE time totals is less than 2 hours
-                                         else if (RunTotalTime < 120){
-                                            //set image to bronze
-                                            TotalRideDistance.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.bronze_ride_time));
-                                            Toast.makeText(getContext(), "bronze", Toast.LENGTH_LONG).show();
-                                         }
-
-                                         //if statement to set image to default when RIDE time total equals 0
-                                        else if (RunTotalTime == 0){
-                                            //set image to default if no data is retrieved
-                                             TotalRideDistance.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.default_medal));
-                                            Toast.makeText(getContext(), "no data", Toast.LENGTH_LONG).show();
-                                        }
-
-
-                                        //Get all-time Run elevation_gain
-                                        Double TotalRunElevationGain = stravaresponse.getAll_run_totals_elevation();
-                                        Double RunTotalElevation = TotalRunElevationGain;
-
-                                        //if (RunTotalElevation >= 10.00){
-                                            //TotalRunElevationGain.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.default_medal));
-
-
-
-
-                                        // Get all-time Ride elevation-gain
-                                        Double TotalRideElevationGain = stravaresponse.getAll_ride_totals_elevation();
-                                        Double RideTotalElevation = TotalRideElevationGain;
-
-                                        //put if statement here\\
 
                                     }
-                                }
+
+                                    }
+
 
 
                             @Override
