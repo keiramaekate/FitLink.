2e596d51d457338e7d91c1d084cc8870e22a655c
Karim Nagy <kn271@kent.ac.uk>
2021-04-15 16:20:14 +0200

    FINAL VERSION

--- app/src/main/java/com/example/fitlinkv3/NutritionPage.java
+++ app/src/main/java/com/example/fitlinkv3/NutritionPage.java
@@ -3,16 +3,21 @@ package com.example.fitlinkv3;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
 //import com.example.fitlinkv3.AddingShat;
@@ -36,49 +41,79 @@ public class NutritionPage extends AppCompatActivity {
     FloatingActionButton buttonadd;
     Button button;
 
+    LinearLayout Recipe1;
+    LinearLayout Recipe2;
+    LinearLayout Recipe3;
+    LinearLayout Recipe4;
+
     LinearLayout food2;
     @Override
 //    protected void onCreate(Bundle savedInstanceState) {
 //        super.onCreate(savedInstanceState);
 
         protected void onCreate(Bundle bundle) {
-            super.onCreate(bundle);
-            setContentView(R.layout.activity_nutrition_page);
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_nutrition_page);
 
-            //button to new activity
-            button = (Button) this.findViewById(R.id.buttonadd);
-            button.setOnClickListener(v -> {
+        //toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-                Intent i = new Intent(NutritionPage.this,AddingShat.class);
-                NutritionPage.this.startActivity(i);
+        //button to new activity
+        button = (Button) this.findViewById(R.id.buttonadd);
+        button.setOnClickListener(v -> {
 
-            });
+            Intent i = new Intent(NutritionPage.this, AddingShat.class);
+            NutritionPage.this.startActivity(i);
 
-//        //ImageButton food = (ImageButton) findViewById(R.id.food);
-//        //food.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//                String food = "https://www.strava.com/challenges/1704?hl=en-GB";
-//                Uri website = Uri.parse(food);
-//
-//                Intent openfood = new Intent(Intent.ACTION_VIEW, website);
-//                if (!(openfood.resolveActivity(getPackageManager()) == null)) {
-//                    startActivity(openfood);
-//                }
-//            }
-//        });
+        });
 
-//        food2 = findViewById(R.id.articleTwo);
-//        food2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String food2 = "https://realfood.tesco.com/meal-planner/view/seasonal-suppers-march.html";
-//                Uri website = Uri.parse(food2);
-//                Intent openfood = new Intent(Intent.ACTION_VIEW, website);
-//                startActivity(openfood);
-//            }
-//        });
+        //set linearlayouts and make them clickable
+        Recipe1 = findViewById(R.id.Recipe1);
+        Recipe1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String food = "https://realfood.tesco.com/meal-planner/view/seasonal-suppers-march.html";
+                Uri website = Uri.parse(food);
+
+                Intent openfood = new Intent(Intent.ACTION_VIEW, website);
+                startActivity(openfood);
+            }
+        });
+        Recipe2 = findViewById(R.id.Recipe2);
+        Recipe2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String food = "https://realfood.tesco.com/meal-planner/view/seasonal-suppers-march.html";
+                Uri website = Uri.parse(food);
+
+                Intent openfood = new Intent(Intent.ACTION_VIEW, website);
+                startActivity(openfood);
+            }
+        });
+        Recipe3 = findViewById(R.id.Recipe3);
+        Recipe3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String food = "https://realfood.tesco.com/meal-planner/view/seasonal-suppers-march.html";
+                Uri website = Uri.parse(food);
+
+                Intent openfood = new Intent(Intent.ACTION_VIEW, website);
+                startActivity(openfood);
+            }
+        });
+        Recipe4 = findViewById(R.id.Recipe1);
+        Recipe4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String food = "https://realfood.tesco.com/meal-planner/view/seasonal-suppers-march.html";
+                Uri website = Uri.parse(food);
+
+                Intent openfood = new Intent(Intent.ACTION_VIEW, website);
+                startActivity(openfood);
+            }
+        });
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
 
@@ -114,13 +149,33 @@ public class NutritionPage extends AppCompatActivity {
                 return false;
             }
         });
-//                // button doesnt open page
-//                Button buttonadd = (Button)
-//                        findViewById(R.id.buttonadd);
+
+    }
+    //set menu to inflate
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    // code to inflate the menu items
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.Settings:
+                Toast.makeText(getApplicationContext(), "You selected settings", Toast.LENGTH_LONG).show();
+                Intent Settings = new Intent(this,Settings.class);
+                this.startActivity(Settings);
+                return true;
+            case R.id.FAQs:
+                Toast.makeText(getApplicationContext(), "You selected FAQs", Toast.LENGTH_LONG).show();
+                Intent FAQs = new Intent(this,FAQs.class);
+                this.startActivity(FAQs);
+                return true;
+            default:
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
 
 
-
-
-}
